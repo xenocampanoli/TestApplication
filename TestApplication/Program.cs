5b59b8c2bf947842ae1127cf99acc23ce4520530
Xeno Campanoli <xeno@eskimo.com>
2016-09-13 13:47:49 -0700

    Test commit three.

--- TestApplication/Program.cs
+++ TestApplication/Program.cs
@@ -10,7 +10,7 @@ namespace TestApplication
     {
         static void Main(string[] args)
         {
-            // Note:  Something added.
+            // Note:  Something added. And a bit more.
         }
     }
 }
